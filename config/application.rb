9f061270bbbbdb2783847f390bd2a361a4fc0d56
Ryutaro95 <ryutaro9595@gmail.com>
2020-08-06 02:25:27 +0900

    rspecファイル生成設定を変更

--- config/application.rb
+++ config/application.rb
@@ -20,13 +20,6 @@ module Myapp
       g.javascripts false
       g.helper false
       g.test_framework false
-      g.test_framework :rspec,
-        view_specs: false,
-        helper_specs: false,
-        controller_specs: false,
-        routing_specs: false,
-        request_specs: false
-      end
     end
   end
 end
